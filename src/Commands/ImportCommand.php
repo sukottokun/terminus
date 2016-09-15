2f155818c6178c4e2094b93ea96eccfb54b917a2
Kyle Sorus <kocherek@gmail.com>
2016-09-15 16:13:01 -0700

    wip

--- src/Commands/ImportCommand.php
+++ src/Commands/ImportCommand.php
@@ -25,3 +25,31 @@ class ImportCommand extends TerminusCommand
     }
 
 }
+
+
+    $site = $this->sites->get(
+      $this->input()->siteName(['args' => $assoc_args,])
+    );
+    $url = $this->input()->string(
+      [
+        'args'     => $assoc_args,
+        'key'      => 'url',
+        'message'  => 'URL of archive to import',
+        'required' => true,
+      ]
+    );
+    $message  = 'Are you sure you want to import this archive?';
+    $message .= ' The dev environment of %s will be overwritten.';
+    $this->input()->confirm(
+      [
+        'message' => $message,
+        'context' => $site->get('name'),
+        'args'    => $assoc_args,
+      ]
+    );
+    $workflow = $site->environments->get('dev')->import($url);
+    $workflow->wait();
+    $this->workflowOutput(
+      $workflow,
+      ['success' => 'Imported site onto Pantheon',]
+    );
