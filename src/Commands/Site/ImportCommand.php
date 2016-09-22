ea3b26fc21000f2cdfb2c0fed697f488e24d56cf
Kyle Sorus <kocherek@gmail.com>
2016-09-22 09:45:07 -0700

    WIP

--- src/Commands/Site/ImportCommand.php
+++ src/Commands/Site/ImportCommand.php
@@ -1,8 +1,9 @@
 <?php
-
 namespace Pantheon\Terminus\Commands\Site;
 use Pantheon\Terminus\Commands\TerminusCommand;
-
+use Terminus\Collections\Sites;
+use Terminus\Models\Environment;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 class ImportCommand extends TerminusCommand
 {
     /**
@@ -15,42 +16,37 @@ class ImportCommand extends TerminusCommand
      *
      * @name site:import
      * @alias import
-     *
+     * @field-labels
+     *   site: Environment
+     *   url: Parameter
      * @option string $site Name of the site to import to
      * @option string $url  URL at which the import archive exists
      * @usage terminus import --site=<site_name> --url=<archive_url>
      *   Imports the file at the archive URL to the site named.
      */
-    public function import(array $options = []) {
+    public function import(array $options = ['site' => null, 'url' => null,]) {
+        // hook into sites 
+        $sites = new Sites();
+        // get site
+        $site = $sites->get($options['site']);
+        // set up url
+        $url = $options['url'];
+        // always import to dev
+        $workflow = $site->environments->get('dev')->import($url);
+        //wait for workflow to do it's thing
+        $workflow->wait();
+        //check the output and respond with the correct notice type:
+        if ($workflow->get('active_description')) {
+            $this->log()->notice("Success");
+        }
+        if ($workflow->get('final_task')->reason) {
+            new TerminusException();
+        }
 
-    }
+    }   
 
 }
 
 
-    // $site = $this->sites->get(
-    //   $this->input()->siteName(['args' => $assoc_args,])
-    // );
-    // $url = $this->input()->string(
-    //   [
-    //     'args'     => $assoc_args,
-    //     'key'      => 'url',
-    //     'message'  => 'URL of archive to import',
-    //     'required' => true,
-    //   ]
-    // );
-    // $message  = 'Are you sure you want to import this archive?';
-    // $message .= ' The dev environment of %s will be overwritten.';
-    // $this->input()->confirm(
-    //   [
-    //     'message' => $message,
-    //     'context' => $site->get('name'),
-    //     'args'    => $assoc_args,
-    //   ]
-    // );
-    // $workflow = $site->environments->get('dev')->import($url);
-    // $workflow->wait();
-    // $this->workflowOutput(
-    //   $workflow,
-    //   ['success' => 'Imported site onto Pantheon',]
-    // );
+
+
--- tests/new_unit_tests/Commands/Site/ImportCommandTest.php
+++ tests/new_unit_tests/Commands/Site/ImportCommandTest.php
@@ -1,29 +1,122 @@
 <?php
-namespace Pantheon\Terminus\UnitTests\Commands;
 
-class importCommandTest extends CommandTestCase
+namespace Pantheon\Terminus\UnitTests\Commands\Connection;
+
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Pantheon\Terminus\Commands\Connection\InfoCommand;
+use Pantheon\Terminus\Config;
+
+use Prophecy\Prophet;
+use Terminus\Models\Environment;
+use Terminus\Models\Site;
+use VCR\VCR;
+
+/**
+ * Test suite for class for Pantheon\Terminus\Commands\Connection\InfoCommand
+ */
+class InfoCommandTest extends ConnectionCommandTest
 {
+    private $prophet;
 
-    public function setUp()
+    /**
+     * Test suite setup
+     *
+     * @return void
+     */
+    protected function setup()
     {
         parent::setUp();
-        $this->setInput(['command' => 'import', 'name' => 'test']);
+
+        $this->command = new InfoCommand($this->getConfig());
+        $this->command->setLogger($this->logger);
+        $this->prophet = new Prophet;
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->prophet->checkPredictions();
+    }
+
+
+    /**
+     * Exercises site:import command with a valid site
+     *
+     * @return void
+     *
+     * @vcr site_connection-info
+     */
+    public function testSiteImport()
+    {
+
+    }
+    /**
+     * Exercises connection:info command invalid site
+     *
+     * @return void
+     */
+    public function testSiteImportInvalidSite()
+    {
+
     }
 
     /**
-     * @test
+     * Exercises the environmentParams protected method
+     *
+     * @return void
      */
-    public function import_command_requests_workflow_when_a_site_exists()
+    public function testEnvironmentParams()
     {
-       $this->assertEquals('Hello World!', $this->runCommand()->fetchTrimmedOutput());
-   }
+        $site_prophet = $this->prophet->prophesize(Site::class);
+        $site_prophet->get('name')->willReturn('my_site');
+        $site = $site_prophet->reveal();
+
+        $env_prophet = $this->prophet->prophesize(Environment::class);
+        $env_prophet->connectionInfo()->willReturn([
+            'param_a' => 'value_a',
+            'param_b' => 'value_b',
+        ]);
+        $environment       = $env_prophet->reveal();
+        $environment->id   = 'my_env';
+        $environment->site = $site;
+
+        // should return all parameters
+        $this->assertEquals(
+            [
+                ['env' => 'my_site.my_env', 'param' => 'param_a', 'value' => 'value_a'],
+                ['env' => 'my_site.my_env', 'param' => 'param_b', 'value' => 'value_b'],
+            ],
+            $this->protectedMethodCall($this->command, 'environmentParams', [$environment])
+        );
+    }
 
     /**
-     * @test
+     * Exercises the environmentParams protected method with a filter argument
+     *
+     * @return void
      */
-    public function import_command_requests_workflow_when_a_site_exists()
+    public function testEnvironmentParamsFilter()
     {
-        $this->setInput(['command' => 'art', 'name' => 'foo']);
-        $this->assertEquals('Not a valid work of art!', $this->runCommand()->fetchTrimmedOutput());
+        $site_prophet = $this->prophet->prophesize(Site::class);
+        $site_prophet->get('name')->willReturn('my_site');
+        $site = $site_prophet->reveal();
+
+        $env_prophet = $this->prophet->prophesize(Environment::class);
+        $env_prophet->connectionInfo()->willReturn([
+            'param_a' => 'value_a',
+            'param_b' => 'value_b',
+        ]);
+        $environment       = $env_prophet->reveal();
+        $environment->id   = 'my_env';
+        $environment->site = $site;
+
+        // should return only filtered parameter
+        $this->assertEquals(
+            [
+                ['env' => 'my_site.my_env', 'param' => 'param_b', 'value' => 'value_b'],
+            ],
+            $this->protectedMethodCall($this->command, 'environmentParams', [$environment, 'param_b'])
+        );
     }
 }
